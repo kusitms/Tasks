eb08f18021691bc3cf8fb2de9effeee96c06268a
leemik3 <57309696+leemik3@users.noreply.github.com>
2020-03-11 17:37:57 +0900

    Update simpledataset.py

--- Week1/simpledataset.py
+++ Week1/simpledataset.py
@@ -3,34 +3,45 @@ import numpy as np
 import pandas as pd
 
 class MyDataset(data.Dataset):
+    
     def __init__(self):
         super().__init__()
-        ```
-        Your code should be written here
-        ```
+        self.df = pd.read_csv("classscore.csv")
+        
     def __len__(self):
-        ```
-        Your code should be written here
-        ```
+        return self.df.shape[0]
+        
     def __getitem__(self, index):
-        ```
-        Your code should be written here
-        ```
+        item = self.df.loc[index]
+        return item
 
 class MyDatasetAdvanced(data.Dataset):
+    
     def __init__(self, mode):
         super().__init__()
-        ```
-        Your code should be written here
-        ```
+        self.df = pd.read_csv("classscore.csv")
+        
+        if mode == 'train':
+            self.df2 = self.df.sample(frac=0.7)
+            
+        elif mode == 'val':
+            self.df2 = self.df.sample(frac=0.1)
+            
+        elif mode == 'test':
+            self.df2 = self.df.sample(frac=0.2)
+        
+        else:
+            print("다시")
+            
     def __len__(self):
-        ```
-        Your code should be written here
-        ```
+        return self.df2.shape[0] 
+    
     def __getitem__(self, index):
-        ```
-        Your code should be written here
-        ```
+        item = self.df2.loc[index]
+        return item
+
+
+
 
 if __name__ == '__main__':
     mydataset = MyDataset()
