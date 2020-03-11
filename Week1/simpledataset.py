417e6ca22a60a4d4356c72db454006dcee56dd40
nsd96 <nsd26534613@gmail.com>
2020-03-11 23:49:03 +0900

    1

--- Week1/simpledataset.py
+++ Week1/simpledataset.py
@@ -2,35 +2,42 @@ from torch.utils import data
 import numpy as np
 import pandas as pd
 
+data_cs = pd.read_csv('C:/Users/nsd96/final_week1/Week1/classscore.csv',encoding='utf-8')
+
+
 class MyDataset(data.Dataset):
     def __init__(self):
         super().__init__()
-        ```
-        Your code should be written here
-        ```
+
+        self.sample_array = data_cs
+
     def __len__(self):
-        ```
-        Your code should be written here
-        ```
+        return len(self.sample_array)
+
     def __getitem__(self, index):
-        ```
-        Your code should be written here
-        ```
+        item = self.sample_array.iloc[index, :]
+        return item
+
 
 class MyDatasetAdvanced(data.Dataset):
     def __init__(self, mode):
         super().__init__()
-        ```
-        Your code should be written here
-        ```
+        self.sample_array = data_cs
+        self.mode = mode
+        if mode == 'train':
+            self.sample_array = self.sample_array.iloc[:21, :]
+        elif mode == 'val':
+            self.sample_array = self.sample_array.iloc[21:24, :]
+        elif mode == 'test':
+            self.sample_array = self.sample_array.iloc[24:, :]
+        print(self.sample_array)
+
     def __len__(self):
-        ```
-        Your code should be written here
-        ```
+        return len(self.sample_array(mode))
+
     def __getitem__(self, index):
-        ```
-        Your code should be written here
-        ```
+        item = self.sample_array.iloc[index, :]
+        return item
 
 if __name__ == '__main__':
     mydataset = MyDataset()
