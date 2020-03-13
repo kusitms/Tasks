85a4b000400ec97c7eeb0e9f0b485981df47a336
dgfghsjd <55286735+dgfghsjd@users.noreply.github.com>
2020-03-13 22:48:27 +0900

    asdasd

---
+++ Week1/TESTTTTT.py
@@ -0,0 +1,40 @@
+from torch.utils import data
+import numpy as np
+import pandas as pd
+
+class MyDataset(data.Dataset):
+    def __init__(self):
+        super().__init__()
+        ```
+        Your code should be written here
+        ```
+    def __len__(self):
+        ```
+        Your code should be written here
+        ```
+    def __getitem__(self, index):
+        ```
+        Your code should be written here
+        ```
+
+class MyDatasetAdvanced(data.Dataset):
+    def __init__(self, mode):
+        super().__init__()
+        ```
+        Your code should be written here
+        ```
+    def __len__(self):
+        ```
+        Your code should be written here
+        ```
+    def __getitem__(self, index):
+        ```
+        Your code should be written here
+        ```
+
+if __name__ == '__main__':
+    mydataset = MyDataset()
+    print(mydataset[0])
+    advanced_dataset_train = MyDatasetAdvanced(mode='train')
+    advanced_dataset_val = MyDatasetAdvanced(mode='val')
+    advanced_dataset_test = MyDatasetAdvanced(mode='test')
