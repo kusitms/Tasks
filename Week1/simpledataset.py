054eace5aabcd62e9440d9ce1231388e06eb9ade
hongchal <chal405@naver.com>
2020-03-14 04:27:15 +0900

    week1
    과제

--- Week1/simpledataset.py
+++ Week1/simpledataset.py
@@ -1,3 +1,4 @@
+
 from torch.utils import data
 import numpy as np
 import pandas as pd
@@ -5,36 +6,49 @@ import pandas as pd
 class MyDataset(data.Dataset):
     def __init__(self):
         super().__init__()
-        ```
-        Your code should be written here
-        ```
+        #self.csv_data = numpy.loadtxt('classsocore'.csv, delimiter=',')
+        self.df = pd.read_csv('./classscore.csv')
+        #print(self.df.head())
+        feature_names = ['Task{}'.format(i) for i in range(1,61)] 
+        #print(feature_names)
+        #print(self.df)
+        #print(self.df['Task1'])
+        #print(self.df[feature_names])
+        self.lables = self.df[feature_names].to_numpy()
+        #print(self.lables)
+        
     def __len__(self):
-        ```
-        Your code should be written here
-        ```
+        return len(self.df)
     def __getitem__(self, index):
-        ```
-        Your code should be written here
-        ```
+        return self.lables[index, :]
 
 class MyDatasetAdvanced(data.Dataset):
-    def __init__(self, mode):
+   def __init__(self, mode):
         super().__init__()
-        ```
-        Your code should be written here
-        ```
-    def __len__(self):
-        ```
-        Your code should be written here
-        ```
-    def __getitem__(self, index):
-        ```
-        Your code should be written here
-        ```
+        if mode=='train':
+            self.df = pd.read_csv('./classscore.csv',skiprows=0, nrows=21)
+        elif mode == 'val':
+            self.df = pd.read_csv('./classscore.csv',skiprows=(1,21), nrows=3)
+        elif mode == 'test':
+            self.df = pd.read_csv('./classscore.csv',skiprows=(1,25), nrows=6)
+        feature_names = ['Task{}'.format(i) for i in range(1,61)]
+        self.labels=self.df[feature_names].to_numpy()
+
+   def __len__(self):
+        return len(self.df)
+
+   def __getitem__(self, index):
+       return self.labels[index,:]
+       
 
 if __name__ == '__main__':
     mydataset = MyDataset()
     print(mydataset[0])
+    #print(mydataset.__getitem__(0)) 
+    #data_list = [[1,1,1],[2,2,2],[3,3,3]]
+    #arr=np.array(data_list)
+    #print(arr)
+    
     advanced_dataset_train = MyDatasetAdvanced(mode='train')
     advanced_dataset_val = MyDatasetAdvanced(mode='val')
     advanced_dataset_test = MyDatasetAdvanced(mode='test')
