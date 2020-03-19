707e5b0f781177a8af81ae3f752a360a54bc675e
nsd96 <nsd26534613@gmail.com>
2020-03-20 02:03:43 +0900

    1

--- Week2/simple_trainer.py
+++ Week2/simple_trainer.py
@@ -1,53 +1,53 @@
+import torch
 import torch.nn as nn
 from torch.utils import data
 import numpy as np
 
 class MyDataset(data.Dataset):
     def __init__(self, mode):
+        super().__init__()
         if mode == 'train':
             self.df = np.random.randint(20, size=(800, 21))
         elif mode == 'val':
             self.df = np.random.randint(20, size=(200, 21))
-        self.features = # Implement code here
-        self.labels = # Implement code here
 
-        """
-            The last column should be the label.
-            First 20 columns will be the features(Input to the model)
-        """
-    
+        self.features = self.df[:, :20]
+        self.labels = self.df[:, -1]
+
     def __len__(self):
         return len(self.df)
-        
+
     def __getitem__(self, index):
-        return torch.FloatTensor(self.features[index, :]), torch.FloatTensor(self.lables[index])
-    
+        return torch.FloatTensor(self.features[index, :]), self.labels[index] #FloatTensor(labels) => error...
+
+
 class MyModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = # Implement code here
+        linear1 = nn.Linear(20, 10, bias=True)
+        linear2 = nn.Linear(10, 10, bias=True)
+        linear3 = nn.Linear(10, 1, bias=True)
+        relu = nn.ReLU()
+        self.model = nn.Sequential(
+            linear1, relu, linear2, relu, linear3
+        )
 
-        """
-            Considering the feature and label constraint mentioned in MyDataset
-            Only model input dimension size and output dimension size are fixed.
-            Activation function and hidden layer dimensions are free.
-        """
-        
     def forward(self, features):
         result = self.model(features)
         return result
+
 
 class MyTrainer:
     def __init__(self, batch_size, lr, total_epochs):
         self.model = MyModel()
         self.train_dataset = MyDataset(mode='train')
         self.val_dataset = MyDataset(mode='val')
-        self.train_datagenerator = data.DataLoader(self.train_dataset, batch_size=batch_size)
-        self.val_datagenerator = data.DataLoader(self.val_dataset, batch_size=batch_size)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
-        self.loss = nn.MSELoss(reduce=False)
+        self.train_datagenerator = data.DataLoader(self.train_dataset, batch_size, drop_last=True)
+        self.val_datagenerator = data.DataLoader(self.val_dataset, batch_size, drop_last=True)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr)
+        self.loss = nn.MSELoss()
         self.total_epochs = total_epochs
-    
+
     def update(self, loss):
         self.optimizer.zero_grad()
         loss.mean().backward()
@@ -56,15 +56,19 @@ class MyTrainer:
     def train(self):
         for epoch in range(self.total_epochs):
             for feature, label in self.train_datagenerator:
-                """
-                    Implement training part.
-                """
+                hypothesis = self.model(feature)
+                loss = self.loss(hypothesis, label.float())
+                self.update(loss)
+            if epoch % 10 == 0:
+                print('Train -- Epoch: {} Loss: {}'.format(epoch,loss))
+
             for feature, label in self.val_datagenerator:
-                """
-                    Implement validation part.
-                """
-        
+                hypothesis = self.model(feature)
+                loss = self.loss(hypothesis, label.float())
+            if epoch % 10 == 0:
+                print('Val -- Epoch: {} Loss: {}'.format(epoch, loss))
+                print('')
+
 if __name__ == '__main__':
-    """
-        Implement code here.
-    """
+    trainer = MyTrainer(8, 0.001, 100)
+    trainer.train()
